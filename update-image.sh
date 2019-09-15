326c802433d54b6779ddad63b5421eb66d2c1af9
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-09-15 23:36:05 +0900

    Fix: commit command in update-image.sh

--- update-image.sh
+++ update-image.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 set -eux
 
 target=$1
@@ -6,6 +7,6 @@ make dockerfile dir=${target}
 
 git status
 git add -A ${target}
-git diff --quiet && git diff --staged --quiet || git commit m "[skip ci] Update ${target} image"
+git diff --quiet && git diff --staged --quiet || git commit -m "[skip ci] Update ${target} image"
 
 make -s new-tags dir=${target} | xargs -ITAG make push dir=${target} tag=TAG
