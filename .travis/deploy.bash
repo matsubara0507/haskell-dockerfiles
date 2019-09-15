81d29142b42dc1ec8a092833f418ee390bf8c54e
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2019-09-16 00:40:23 +0900

    Fix: deploy bash

--- .travis/deploy.bash
+++ .travis/deploy.bash
@@ -8,7 +8,5 @@ chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
 
 # commit the assets in docs/ if changed, and push to GitHub using SSH
-git config user.name "${GIT_NAME}"
-git config user.email "${GIT_EMAIL}"
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
-git checkout master
+git push -u origin $1
